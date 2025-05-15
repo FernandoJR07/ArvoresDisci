c7b31d1ff165aa378a7766cde73f8474164afb82
FernandoJR07 <ffcostajr@outlook.com>
2025-05-15 20:14:52 -0300

    Primeiro commit

---
+++ arvore.java
@@ -0,0 +1,5 @@
+public class arvore {
+
+    no raiz;
+
+}
---
+++ no.java
@@ -0,0 +1,12 @@
+public class no {
+    
+    String conteudo;
+
+    no esquerda, direita;
+
+    public no(String conteudo) {
+        this.conteudo = conteudo;
+        esquerda = direita = null;
+    }
+
+}
